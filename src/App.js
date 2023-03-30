677f9ab4d7905e3fe1630755b80a84d3a9eed4ac
Rahul <dubeyrahul890@gmail.com>
2023-03-31 00:04:51 +0530

    minor update with components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MyButton from './components/buttonLayout';
+import navigationBar from './components/layouts/headerComponents';
+
+
+const welcome = {
+  title: 'Secure Chat',
+  describe: 'Securely chat with us now',
+  link : 'https://i.imgur.com/yXOvdOSs.jpg'
+};
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Hello World
-        </p>
-      </header>
+       
+      <div className="row">
+      <div>
+      <p>
+          {welcome.describe}
+      </p>
+      </div>
+      <MyButton />
+      <navigationBar />
+      </div>
     </div>
   );
 }
---
+++ src/components/buttonLayout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+function MyButton() {
+  return (
+    <div>
+     <Button>Let Get start</Button>
+    </div>
+  );
+}
+
+export default MyButton;
